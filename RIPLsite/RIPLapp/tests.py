6d7d1d58dd6980648c20d6cca1ef26057b43e4d7
Mayank Kapoor <mayankkapoormail@gmail.com>
2015-04-02 11:53:29 +0530

    Added RIPLapp, functional_test folder

---
+++ RIPLsite/RIPLapp/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+
+
+# Create your tests here.
