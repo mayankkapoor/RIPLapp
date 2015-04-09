1082601e70267d3ac4b424743afc1e8dd29c3ff5
Anshu Prateek <anshu.prateek@ril.com>
2015-04-09 16:23:53 +0530

    screen 2-4 + code refactoring

--- RIPLsite/RIPLapp/urls.py
+++ RIPLsite/RIPLapp/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 urlpatterns = [  # Examples:  # url(r'^$', 'RIPLsite.views.home', name='home'),
                  url(r'screen1/$', 'RIPLapp.views.screen1login_response', name='screen1'),
                  url(r'screen2/$', 'RIPLapp.views.screen2bus_safe_response', name='screen2'),
+                 url(r'screen3/$', 'RIPLapp.views.screen3bus_supply_count', name='screen3'),
+                 url(r'screen4/$', 'RIPLapp.views.screen4bus_started_depot', name='screen4'),
                  url(r'screen5/$', 'RIPLapp.views.screen5_total_people_picked', name='screen5')
 
 ]
